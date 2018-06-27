36093656456063f4dd92b01f5d0b678d78afa0b9
Ganaa Mongol <ganaa7@gmail.com>
2018-06-27 18:41:53 +0800

    all files added in git folder

---
+++ test.php
@@ -0,0 +1,34 @@
+<?php
+
+$name = "test";
+
+
+funciton test(){
+
+    $var  = 'My variable here';
+
+
+    $var2 = 'its new text form here'
+
+
+}
+
+
+class Test {
+
+  $var1= 'test';
+
+  public function name($value='')
+  {
+      $value = 1;
+
+      return $value;
+  }
+
+}
+
+ $new = new Test;
+
+ $new->name();
+
+?>
